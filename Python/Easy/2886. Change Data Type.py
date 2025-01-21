162e6b40fec31fba42c8c2c2f704ed79277a7632
Anthony T <anthonyt3@sbcglobal.net>
2025-01-21 13:54:18 -0800

    Alternate Solution For 2886 Change Data Types

--- Python/Easy/2886. Change Data Type.py
+++ Python/Easy/2886. Change Data Type.py
@@ -3,3 +3,8 @@ import pandas as pd
 def changeDatatype(students: pd.DataFrame) -> pd.DataFrame:
     students = students.astype({"grade": int})
     return students
+
+# Improves memory at the expense of performance
+# def changeDatatype(students: pd.DataFrame) -> pd.DataFrame:
+#     students = students.astype({"grade": int}, False)
+#     return students
