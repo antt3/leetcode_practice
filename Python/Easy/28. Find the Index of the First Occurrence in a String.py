674cac44b056a055261bb5ed3c0d5a5dab2b7b2b
Anthony T <anthonyt3@sbcglobal.net>
2025-01-21 14:41:46 -0800

    Initial Solution Without Try/Except For Find the Index of the First Occurrence in a String

--- Python/Easy/28. Find the Index of the First Occurrence in a String.py
+++ Python/Easy/28. Find the Index of the First Occurrence in a String.py
@@ -1,6 +1,14 @@
 class Solution:
     def strStr(self, haystack: str, needle: str) -> int:
-        try:
-            return haystack.index(needle)
-        except:
-            return -1
+        for i in range(len(haystack)):
+            for j in range(i + 1, len(haystack) + 1):
+                if haystack[i:j] == needle:
+                    return i
+        return -1
+
+    # Fastest runtime, but uses try/except
+    # def strStr(self, haystack: str, needle: str) -> int:
+    #     try:
+    #         return haystack.index(needle)
+    #     except:
+    #         return -1
