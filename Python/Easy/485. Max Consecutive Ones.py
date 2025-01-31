c86d7c4ccb6ca10f9fec81b6ec8f40893faa72a4
Anthony T <anthonyt3@sbcglobal.net>
2025-01-30 22:27:47 -0800

    Added A Better Solution In Python For Max Consecutive Ones

--- Python/Easy/485. Max Consecutive Ones.py
+++ Python/Easy/485. Max Consecutive Ones.py
@@ -1,24 +1,35 @@
 class Solution:
     def findMaxConsecutiveOnes(self, nums: List[int]) -> int:
-        if len(nums) <= 2 or 0 not in nums:
-            return sum(nums)
-        out = 0
-        l = 0
-        while l < len(nums) - 1:
-            if nums[l] != 1:
-                l += 1
-                continue
-            temp = 1
-            r = l + 1
-            while r < len(nums):
-                if nums[r] != 1:
-                    l = r
-                    if out < temp:
-                        out = temp
-                    break
-                temp += 1
-                r += 1
-                if out < temp:
-                    out = temp
-            l += 1
-        return out
+        # Found Better Solution
+        maxi = result = 0
+        for num in nums:
+            if num == 1:
+                result += 1
+                maxi = max(maxi, result)
+            else:
+                result = 0
+        return maxi
+        
+        # Initial Solution
+        # if len(nums) <= 2 or 0 not in nums:
+        #     return sum(nums)
+        # out = 0
+        # l = 0
+        # while l < len(nums) - 1:
+        #     if nums[l] != 1:
+        #         l += 1
+        #         continue
+        #     temp = 1
+        #     r = l + 1
+        #     while r < len(nums):
+        #         if nums[r] != 1:
+        #             l = r
+        #             if out < temp:
+        #                 out = temp
+        #             break
+        #         temp += 1
+        #         r += 1
+        #         if out < temp:
+        #             out = temp
+        #     l += 1
+        # return out
