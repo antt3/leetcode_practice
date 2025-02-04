fa777108af55072712f08a15d4da5d05df43c2bd
Anthony T <anthonyt3@sbcglobal.net>
2025-02-04 00:38:56 -0800

    Added My Refactored Solution In Python For Remove Duplicates from Sorted Array

--- Python/Easy/26. Remove Duplicates from Sorted Array.py
+++ Python/Easy/26. Remove Duplicates from Sorted Array.py
@@ -1,9 +1,18 @@
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
-        i, k = 0, 1
+        # Refactored Solution
+        i = 1
         for j in range(1, len(nums)):
-            if nums[i] < nums[j]:
-                nums[i + 1] = nums[j]
+            if nums[i - 1] < nums[j]:
+                nums[i] = nums[j]
                 i += 1
-                k += 1
-        return k
+        return i
+        
+        # Initial Solution
+        # i, k = 0, 1
+        # for j in range(1, len(nums)):
+        #     if nums[i] < nums[j]:
+        #         nums[i + 1] = nums[j]
+        #         i += 1
+        #         k += 1
+        # return k
