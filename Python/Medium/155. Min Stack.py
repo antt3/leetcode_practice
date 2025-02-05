46d5c02bbc7e1fb2bb433077b3450aa21eb6beab
Anthony T <anthonyt3@sbcglobal.net>
2025-02-04 22:33:01 -0800

    Added A Slightly Cleaner Solution In Python For Min Stack

--- Python/Medium/155. Min Stack.py
+++ Python/Medium/155. Min Stack.py
@@ -1,23 +1,45 @@
 class MinStack:
+    # Slightly Cleaner Solution
     def __init__(self):
         self.stack = []
-        self.min = []
+        self.min_stack = []
 
     def push(self, val: int) -> None:
         self.stack.append(val)
-        if not self.min:
-            self.min.append(val)
-        elif self.min[-1] > val:
-            self.min.append(val)
-        else:
-            self.min.append(self.min[-1])
+        self.min_stack.append(min(val, self.min_stack[-1]) if self.min_stack else val)
+        # val = min(val, self.min_stack[-1]) if self.min_stack else val
+        # self.min_stack.append(val)
 
     def pop(self) -> None:
         self.stack.pop()
-        self.min.pop()
+        self.min_stack.pop()
 
     def top(self) -> int:
         return self.stack[-1]
 
     def getMin(self) -> int:
-        return self.min[-1]
+        return self.min_stack[-1]
+
+    # Initial Solution
+    # def __init__(self):
+    #     self.stack = []
+    #     self.min = []
+
+    # def push(self, val: int) -> None:
+    #     self.stack.append(val)
+    #     if not self.min:
+    #         self.min.append(val)
+    #     elif self.min[-1] > val:
+    #         self.min.append(val)
+    #     else:
+    #         self.min.append(self.min[-1])
+
+    # def pop(self) -> None:
+    #     self.stack.pop()
+    #     self.min.pop()
+
+    # def top(self) -> int:
+    #     return self.stack[-1]
+
+    # def getMin(self) -> int:
+    #     return self.min[-1]
