a6241203381ea3dc9113f2c8cadeab450fcc9703
Anthony T <anthonyt3@sbcglobal.net>
2025-02-04 22:13:18 -0800

    Replaced My Initial Solution In Python For Min Stack With My Initial Solution With O(1) Runtime For getMin() (A Requirement)

--- Python/Medium/155. Min Stack.py
+++ Python/Medium/155. Min Stack.py
@@ -1,16 +1,23 @@
 class MinStack:
-    # Initial Solution
     def __init__(self):
         self.stack = []
+        self.min = []
 
     def push(self, val: int) -> None:
         self.stack.append(val)
+        if not self.min:
+            self.min.append(val)
+        elif self.min[-1] > val:
+            self.min.append(val)
+        else:
+            self.min.append(self.min[-1])
 
     def pop(self) -> None:
         self.stack.pop()
+        self.min.pop()
 
     def top(self) -> int:
         return self.stack[-1]
 
     def getMin(self) -> int:
-        return min(self.stack)
+        return self.min[-1]
