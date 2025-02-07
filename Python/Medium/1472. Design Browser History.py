f1ace638a4aa541536a6f63ffb2970d84d26f2fa
Anthony T <anthonyt3@sbcglobal.net>
2025-02-06 23:57:20 -0800

    Added My Initial Solution In Python Using An Array For Design Browser History

--- Python/Medium/1472. Design Browser History.py
+++ Python/Medium/1472. Design Browser History.py
@@ -1,37 +1,54 @@
-class DLL:
-    
-    def __init__(self, page, last = None, next = None):
-        self.page = page
-        self.last =  last
-        self.next = next
-
+# Initial Solution Using An Array
 class BrowserHistory:
 
     def __init__(self, homepage: str):
-        self.history = DLL(homepage)
+        self.history = [homepage]
+        self.curr = 0
 
     def visit(self, url: str) -> None:
-        self.history.next = DLL(url, self.history)
-        self.history = self.history.next
+        self.history = self.history[:self.curr + 1]
+        self.history.append(url)
+        self.curr += 1
 
     def back(self, steps: int) -> str:
-        while self.history.last and steps:
-            self.history = self.history.last
-            steps -= 1
-        return self.history.page
+        self.curr = self.curr - steps if self.curr - steps >= 0 else 0
+        return self.history[self.curr]
 
     def forward(self, steps: int) -> str:
-        while steps and self.history.next:
-            self.history = self.history.next
-            steps -= 1
-        return self.history.page
+        self.curr = self.curr + steps if self.curr + steps < len(self.history) else len(self.history) - 1
+        return self.history[self.curr]
+
+
+# Refactored Solution
+# class DLL:
+    
+#     def __init__(self, page, last = None, next = None):
+#         self.page = page
+#         self.last =  last
+#         self.next = next
+
+# class BrowserHistory:
+
+#     def __init__(self, homepage: str):
+#         self.history = DLL(homepage)
+
+#     def visit(self, url: str) -> None:
+#         self.history.next = DLL(url, self.history)
+#         self.history = self.history.next
+
+#     def back(self, steps: int) -> str:
+#         while self.history.last and steps:
+#             self.history = self.history.last
+#             steps -= 1
+#         return self.history.page
+
+#     def forward(self, steps: int) -> str:
+#         while steps and self.history.next:
+#             self.history = self.history.next
+#             steps -= 1
+#         return self.history.page
         
 
-# Your BrowserHistory object will be instantiated and called as such:
-# obj = BrowserHistory(homepage)
-# obj.visit(url)
-# param_2 = obj.back(steps)
-# param_3 = obj.forward(steps)
 # Initial Solution
 # class DLL:
     
