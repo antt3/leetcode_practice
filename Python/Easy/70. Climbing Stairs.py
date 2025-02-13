9dcc05425ca560f909739c1e81d0d6ca077f8031
Anthony T <anthonyt3@sbcglobal.net>
2025-02-12 22:32:26 -0800

    Added An Alternate Solution Solution In Python Using Recursion For Climbing Stairs

--- Python/Easy/70. Climbing Stairs.py
+++ Python/Easy/70. Climbing Stairs.py
@@ -1,5 +1,16 @@
 class Solution:
+    # Initial Solution
     def climbStairs(self, n: int) -> int:
         if n <= 3:
             return n
         return self.climbStairs(n - 2) + self.climbStairs(n - 1)
+
+    # Alternate Solution Using DFS
+    # def climbStairs(self, n: int) -> int:
+
+    #     def dfs(i):
+    #         if i >= n:
+    #             return i == n
+    #         return dfs(i + 1) + dfs(i + 2)
+
+    #     return dfs(0)
