f3de26c0b67df7d1e07e0f3aa89667e221f4ee0f
antt3 <anthonyt3@sbcglobal.net>
2025-04-09 20:04:33 -0700

    Added An Optimal Solution In Rust For Remove Duplicates From Sorted Array

--- Rust/Easy/26. Remove Duplicates from Sorted Array.rs
+++ Rust/Easy/26. Remove Duplicates from Sorted Array.rs
@@ -1,15 +1,25 @@
 pub fn remove_duplicates(nums: &mut Vec<i32>) -> i32 {
-    // Initial Solution
-    if nums.len() == 1 {return 1}
-    let mut k = 1;
-    let mut i = 1;
-    for j in 1..nums.len() {
-        if j == nums.len() {return k}
-        if nums[j] > nums[i - 1] {
-            nums[i] = nums[j];
-            i += 1;
-            k += 1;
+    // Opimized Solution
+    let mut l = 1;
+        for r in 1..nums.len() {
+            if (nums[r] != nums[r - 1]) {
+                nums[l] = nums[r];
+                l += 1;
+            }
         }
-    }
-    k
+        l as i32
+    
+    // Initial Solution
+    // if nums.len() == 1 {return 1}
+    // let mut k = 1;
+    // let mut i = 1;
+    // for j in 1..nums.len() {
+    //     if j == nums.len() {return k}
+    //     if nums[j] > nums[i - 1] {
+    //         nums[i] = nums[j];
+    //         i += 1;
+    //         k += 1;
+    //     }
+    // }
+    // k
 }
