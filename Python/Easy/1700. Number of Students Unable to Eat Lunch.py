b3a62c8b82481a29c6d878c2c87a7e85e4ed1dd1
antt3 <anthonyt3@sbcglobal.net>
2025-05-03 21:31:57 -0700

    Added An Optimized Solution In Python For Number Of Students Unable To Eat Lunch

--- Python/Easy/1700. Number of Students Unable to Eat Lunch.py
+++ Python/Easy/1700. Number of Students Unable to Eat Lunch.py
@@ -1,15 +1,28 @@
 def countStudents(self, students: list[int], sandwiches: list[int]) -> int:
-        # Initial Solution
-        ute = len(students)
-        while sandwiches and ute:
-            temp = ute
-            for i in range(len(students)):
-                if not sandwiches or not ute:
-                    break
-                if students[i] == sandwiches[0]:
-                    del sandwiches[0]
-                    students[i] = 2
-                    ute = ute - 1
-            if temp == ute:
+        # Optimized Solution
+        res = len(students)
+        cnt = Counter(students)
+
+        for s in sandwiches:
+            if cnt[s] > 0:
+                res -= 1
+                cnt[s] -= 1
+            else:
                 break
-        return ute
+        
+        return res 
+        
+        # Initial Solution
+        # ute = len(students)
+        # while sandwiches and ute:
+        #     temp = ute
+        #     for i in range(len(students)):
+        #         if not sandwiches or not ute:
+        #             break
+        #         if students[i] == sandwiches[0]:
+        #             del sandwiches[0]
+        #             students[i] = 2
+        #             ute = ute - 1
+        #     if temp == ute:
+        #         break
+        # return ute
