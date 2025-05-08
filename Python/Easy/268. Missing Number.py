01e02fb92417fa3870eddf6f498647f250c13b06
antt3 <anthonyt3@sbcglobal.net>
2025-05-08 04:44:04 -0700

    Added My RefactoredSolution In Python For Missing Number

--- Python/Easy/268. Missing Number.py
+++ Python/Easy/268. Missing Number.py
@@ -1,8 +1,17 @@
 def missingNumber(self, nums: list[int]) -> int:
-    # Initial Solution
-    nums_count = [0] * (len(nums) + 1)
+    # My Refactored Solution
+    # Time: O(n) Space: O(1)
+    t_num = (len(nums) * (len(nums) + 1)) / 2
+    nums_sum = 0
     for num in nums:
-        nums_count[num] = 1
+        nums_sum += num
+    return int(t_num - nums_sum)
+    
+    # Initial Solution
+    # Time: O(n) Space: O(n)
+    # nums_count = [0] * (len(nums) + 1)
+    # for num in nums:
+    #     nums_count[num] = 1
         
-    for i in range(len(nums_count)):
-        if not nums_count[i]: return i
+    # for i in range(len(nums_count)):
+    #     if not nums_count[i]: return i
