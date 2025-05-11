ae5344547a0ec680588ea0cf2cd621fefe6bdf3e
antt3 <anthonyt3@sbcglobal.net>
2025-05-10 20:11:49 -0700

    Added My Initial Solution In Python For Binary Search

---
+++ Python/Easy/704. Binary Search.py
@@ -0,0 +1,9 @@
+def search(self, nums: list[int], target: int) -> int:
+    # Initial Soluton
+    l, r = 0, len(nums) - 1
+    while l <= r:
+        m = l + (r - l) // 2
+        if nums[m] < target: l = m + 1
+        elif nums[m] > target: r = m - 1
+        else: return m
+    return -1
