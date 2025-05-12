5699e18f81932f4b2c4b4f5f86334d8f9f55bc3c
antt3 <anthonyt3@sbcglobal.net>
2025-05-11 21:54:35 -0700

    Added An Alternate Solution In Python For Guess Number Higher Or Lower

--- Python/Easy/374. Guess Number Higher or Lower.py
+++ Python/Easy/374. Guess Number Higher or Lower.py
@@ -8,3 +8,21 @@ def guessNumber(self, n: int) -> int:
         if pick == -1: r = m - 1
         elif pick == 1: l = m + 1
         else: return m
+
+    # Alternate Solution
+    # Time: O(log3(n)) | Space: O(1)
+    # l, r = 1, n
+    # while True:
+    #     m1 = l + (r - l) // 3
+    #     m2 = r - (r - l) // 3
+    #     if guess(m1) == 0:
+    #         return m1
+    #     if guess(m2) == 0:
+    #         return m2
+    #     if guess(m1) + guess(m2) == 0:
+    #         l = m1 + 1
+    #         r = m2 - 1
+    #     elif guess(m1) == -1:
+    #         r = m1 - 1
+    #     else:
+    #         l = m2 + 1
