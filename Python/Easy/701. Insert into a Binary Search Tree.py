68f9b1ae3b9629ba38fb7ec14b8c31d6a4da3dc6
antt3 <anthonyt3@sbcglobal.net>
2025-05-15 16:54:35 -0700

    Added My Iterative Solution In Python For Insert Into A Binary Search Tree

--- Python/Easy/701. Insert into a Binary Search Tree.py
+++ Python/Easy/701. Insert into a Binary Search Tree.py
@@ -1,9 +1,25 @@
 def insertIntoBST(self, root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
+    # My Iterative Solution
+    # Time: O(log(n)) | Time: O(1)
+    if not root: return TreeNode(val)
+    temp = root
+    while True:
+        if temp.val < val:
+            if not temp.right:
+                temp.right = TreeNode(val)
+                return root
+            temp = temp.right
+        else:
+            if not temp.left:
+                temp.left = TreeNode(val)
+                return root
+            temp = temp.left
+    
     # Initial Solution
     # Time: 0(log(n)) | Space: O(log(n))
-    if not root: return TreeNode(val)
-    if root.val > val:
-        root.left = self.insertIntoBST(root.left, val)
-    else:
-        root.right = self.insertIntoBST(root.right, val)
-    return root
+    # if not root: return TreeNode(val)
+    # if root.val > val:
+    #     root.left = self.insertIntoBST(root.left, val)
+    # else:
+    #     root.right = self.insertIntoBST(root.right, val)
+    # return root
