a33eaf2ef49eb425123024f82f5deb93c4a3565a
antt3 <anthonyt3@sbcglobal.net>
2025-05-14 23:21:08 -0700

    Added An Iterative Solution In Python For Search In A Binary Search Tree

--- Python/Easy/700. Search in a Binary Search Tree.py
+++ Python/Easy/700. Search in a Binary Search Tree.py
@@ -1,6 +1,12 @@
 def searchBST(self, root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
-        # Initial Solution
-        # Time: 0(H) | Space: O(H)
-        if not root or root.val == val: return root
-        if root.val < val: return self.searchBST(root.right, val)
-        return self.searchBST(root.left, val)
+    # Iterative Solution
+    # Time: 0(H) | Space: O(1)
+    while root and root.val != val:
+        root = root.left if val < root.val else root.right
+    return root
+    
+    # Initial Solution
+    # Time: 0(H) | Space: O(H)
+    # if not root or root.val == val: return root
+    # if root.val < val: return self.searchBST(root.right, val)
+    # return self.searchBST(root.left, val)
