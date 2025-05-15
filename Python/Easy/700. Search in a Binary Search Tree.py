95ce745cc61266eb566757e4c4af70e9a971cdcf
antt3 <anthonyt3@sbcglobal.net>
2025-05-14 23:40:32 -0700

    Updated Time/Space Complexity For The Solutions In Python For Search In A Binary Search Tree

--- Python/Easy/700. Search in a Binary Search Tree.py
+++ Python/Easy/700. Search in a Binary Search Tree.py
@@ -1,12 +1,12 @@
 def searchBST(self, root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
     # Iterative Solution
-    # Time: 0(H) | Space: O(1)
+    # Time: 0(log(n)) | Space: O(1)
     while root and root.val != val:
         root = root.left if val < root.val else root.right
     return root
     
     # Initial Solution
-    # Time: 0(H) | Space: O(H)
+    # Time: 0(log(n)) | Space: O(log(n))
     # if not root or root.val == val: return root
     # if root.val < val: return self.searchBST(root.right, val)
     # return self.searchBST(root.left, val)
