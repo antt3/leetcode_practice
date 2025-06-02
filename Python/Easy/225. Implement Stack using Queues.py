659204d921a4ba525c73efd5f68e2a4188bb201e
antt3 <anthonyt3@sbcglobal.net>
2025-06-01 22:59:42 -0700

    Added A Slightly Optimized Version Of push() & An Ideal Solution In Python For Implement Stack Using Queues

--- Python/Easy/225. Implement Stack using Queues.py
+++ Python/Easy/225. Implement Stack using Queues.py
@@ -1,26 +1,52 @@
 
 class MyStack:
-    # Initial Solution
+    # Ideal Solution
     def __init__(self):
-        self.q1 = deque()
-        self.q2 = deque()
+        self.q = None
 
-    # Time: O(n)
+    # Time: O(1)
     def push(self, x: int) -> None:
-        while self.q1:
-            self.q2.append(self.q1.popleft())
-        self.q1.append(x)
-        while self.q2:
-            self.q1.append(self.q2.popleft())
+        self.q = deque([x, self.q])
 
     # Time: O(1)
     def pop(self) -> int:
-        return self.q1.popleft()
+        top = self.q.popleft()
+        self.q = self.q.popleft()
+        return top
 
-    # Time: 0(1)
+    # Time: O(1)
     def top(self) -> int:
-        return self.q1[0]
+        return self.q[0]
 
-    # Time: 0(1)
+    # Time: O(1)
     def empty(self) -> bool:
-        return not self.q1
+        return not self.q
+    
+    # Initial Solution
+    # def __init__(self):
+    #     self.q1 = deque()
+    #     self.q2 = deque()
+    
+    # # Time: O(n)
+    # def push(self, x: int) -> None:
+    #     while self.q1:
+    #         self.q2.append(self.q1.popleft())
+    #     self.q1.append(x)
+    #     # Slightly Optimized Solution
+    #     self.q1, self.q2 = self.q2, self.q1
+
+    #     # Initial Soluton
+    #     # while self.q2:
+    #     #     self.q1.append(self.q2.popleft())
+
+    # # Time: O(1)
+    # def pop(self) -> int:
+    #     return self.q1.popleft()
+
+    # # Time: 0(1)
+    # def top(self) -> int:
+    #     return self.q1[0]
+
+    # # Time: 0(1)
+    # def empty(self) -> bool:
+    #     return not self.q1
