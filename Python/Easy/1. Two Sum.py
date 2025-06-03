837df6f30f637d626f397abd07ec68de0445b109
antt3 <anthonyt3@sbcglobal.net>
2025-06-02 18:49:33 -0700

    Added An Alternate Solution In Python For Two Sum

--- Python/Easy/1. Two Sum.py
+++ Python/Easy/1. Two Sum.py
@@ -1,9 +1,18 @@
 def twoSum(self, nums: list[int], target: int) -> list[int]:
+    # Alternate Solution
+    # Time: O(n) | Space: 0(n)
+    prevMap = {}
+    for i, n in enumerate(nums):
+        diff = target - n
+        if diff in prevMap:
+            return [prevMap[diff], i]
+        prevMap[n] = i
+
     # Initial Solution
     # Time: O(n) | Space: 0(n)
-    hm = {}
-    for i, v in enumerate(nums):
-        for k in hm.keys():
-            if (v + k) == target:
-                return [hm[k], i]
-        hm[v] = i
+    # hm = {}
+    # for i, v in enumerate(nums):
+    #     for k in hm.keys():
+    #         if (v + k) == target:
+    #             return [hm[k], i]
+    #     hm[v] = i
