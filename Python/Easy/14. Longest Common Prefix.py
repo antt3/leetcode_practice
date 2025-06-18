b0d1c7f5a0deb4b3b65bb4bfcd31bc36a41e75d5
antt3 <anthonyt3@sbcglobal.net>
2025-06-17 17:13:00 -0700

    Added An Improved Solution In Python For Longest Common Prefix

--- Python/Easy/14. Longest Common Prefix.py
+++ Python/Easy/14. Longest Common Prefix.py
@@ -1,4 +1,16 @@
 def longestCommonPrefix(self, strs: list[str]) -> str:
+    # Improved Solution
+    # Time: O(n * m) | Space: O(1)
+    prefix = strs[0]
+    for i in range(1, len(strs)):
+        j = 0
+        while j < min(len(prefix), len(strs[i])):
+            if prefix[j] != strs[i][j]:
+                break
+            j += 1
+        prefix = prefix[:j]
+    return prefix
+    
     # Initial Solution
     # Time: O(n * m) | Space: O(m)
     prefix = min(strs, key=len)
