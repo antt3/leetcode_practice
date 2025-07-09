9daa031d99ae2bf1f6a192368f8cb128e6dae36f
antt3 <anthonyt3@sbcglobal.net>
2025-07-08 22:00:07 -0700

    Added My Initial Solution In T-SQL For Combine Two Tables

---
+++ T-SQL/Easy/175. Combine Two Tables.sql
@@ -0,0 +1,9 @@
+/* Initial Solution */
+SELECT
+    Person.firstName,
+    Person.lastName,
+    Address.city,
+    Address.state
+FROM
+    Person
+    LEFT JOIN Address ON Address.personId = Person.PersonId
