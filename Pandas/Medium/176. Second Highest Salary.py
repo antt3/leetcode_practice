313ddc41a02674530f57d5c35b8b3e2acb0ffe9b
antt3 <anthonyt3@sbcglobal.net>
2025-07-09 00:04:47 -0700

    Added A Simplified Solution In Pandas For Second Highest Salary

--- Pandas/Medium/176. Second Highest Salary.py
+++ Pandas/Medium/176. Second Highest Salary.py
@@ -1,6 +1,12 @@
 import pandas as pd
 
 def second_highest_salary(employee: pd.DataFrame) -> pd.DataFrame:
+    # Simplified Solution
+    sorted_salaries = employee['salary'].sort_values(ascending=False).drop_duplicates()
+    return pd.DataFrame({
+        'SecondHighestSalary': [None if sorted_salaries.size < 2 else sorted_salaries.iloc[1]]
+    })
+    
     # Initial Solution
-    res_df = employee.sort_values(by="salary", ascending=False)[["salary"]].drop_duplicates()[1:2].set_axis(["SecondHighestSalary"], axis=1)
-    return pd.DataFrame({"SecondHighestSalary": [None]}) if res_df.empty else res_df
+    # res_df = employee.sort_values(by="salary", ascending=False)[["salary"]].drop_duplicates()[1:2].set_axis(["SecondHighestSalary"], axis=1)
+    # return pd.DataFrame({"SecondHighestSalary": [None]}) if res_df.empty else res_df
